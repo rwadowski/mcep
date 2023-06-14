e7394722953a44bbc157018205b94145eba3e4b4
Robert Wadowski <robert.wadowski@morgenrete.com>
2023-06-14 18:31:46 +0200

    no definition error handling

--- services/src/definition/get.rs
+++ services/src/definition/get.rs
@@ -2,9 +2,12 @@ use definition::Definition;
 use sqlx::{Pool, Postgres};
 
 pub async fn get_app_definition(pool: &Pool<Postgres>, id: i32) -> Result<Definition, String> {
-    let def = sqlx::query_as::<_, Definition>("SELECT * FROM app_definitions WHERE id = $1")
+    let definition_opt = sqlx::query_as::<_, Definition>("SELECT * FROM app_definitions WHERE id = $1")
         .bind(id)
         .fetch_one(pool)
         .await;
-    Ok(def.unwrap())
+    match definition_opt {
+        Ok(def) => Ok(def),
+        Err(err) => Err(err.to_string()),
+    }
 }
