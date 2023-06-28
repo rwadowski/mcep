a3bd44d510ec0164761259a6111952271893cdd5
Robert Wadowski <robert.wadowski@morgenrete.com>
2023-06-28 22:56:37 +0200

    no definition error handling

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -3,10 +3,11 @@ mod definition;
 #[macro_use] extern crate rocket;
 
 use rocket::{Build, Rocket};
+use rocket::figment::Figment;
 use sqlx::{Pool, Postgres};
 
-pub fn start_rocket(pool: Pool<Postgres>) -> Rocket<Build> {
-    rocket::build()
+pub fn start_rocket(config: Figment, pool: Pool<Postgres>) -> Rocket<Build> {
+    rocket::custom(config)
         .manage(pool)
         .mount("/api", routes![
             definition::app::get_app_definition_handler,
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate rocket;
 
+use rocket::figment::providers::{Env, Format, Toml};
 use tokio::signal;
 use database;
 use api;
@@ -11,9 +12,8 @@ async fn main() {
     let database_connection_pool = database::init_connection_pool().await;
 
     database::apply_migrations(&database_connection_pool).await.expect("migrations failed");
-
     tokio::spawn(async move {
-        api::start_rocket(database_connection_pool).launch().await
+        api::start_rocket(rocket_config(), database_connection_pool).launch().await
     });
 
     signal::ctrl_c().await.expect("failed to listen for event");
@@ -21,3 +21,9 @@ async fn main() {
     println!("Closing mcep");
 }
 
+fn rocket_config() -> rocket::figment::Figment {
+    rocket::figment::Figment::from(rocket::config::Config::default()).
+        merge(Toml::file("Rocket.toml").nested()).
+        merge(Env::prefixed("MCEP_").global()).
+        select(rocket::figment::Profile::from_env_or("MCEP_PROFILE", "default"))
+}
