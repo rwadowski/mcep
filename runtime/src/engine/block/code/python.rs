b225c6e6fecdce058864ea2ab69b05d3c7bd6d46
Robert Wadowski <robert.wadowski@morgenrete.com>
2023-09-29 12:47:02 +0200

    wip

--- runtime/src/engine/block/code/python.rs
+++ runtime/src/engine/block/code/python.rs
@@ -1,9 +1,10 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashMap};
 use pyo3::prelude::*;
 use pyo3::types::PyTuple;
+use crate::DataFrame;
 use crate::engine::Data;
 
-struct PythonBlock {
+pub(crate) struct PythonBlock {
     pub code: String
 }
 
@@ -11,17 +12,38 @@ struct PythonBlock {
 impl PythonBlock {
 
     //TODO - initialize python during creation of struct
-    fn run_python_code(&self, input: BTreeMap<String, Data>) -> () {
+    pub fn run_python_code(&self, input: HashMap<String, Data>) -> PyResult<()> {
         Python::with_gil(|py| {
-            let f: Py<PyAny> = PyModule::from_code(py, self.code.as_str())?
+            let function: Py<PyAny> =  PyModule::from_code(py, self.code.as_str(), "", "")?
                 .getattr("logic")?
                 .into();
-            let values = input.values().into_iter();
-            let args = PyTuple::new(py, values);
-            let result = f.call1(py, args);
-                ()
+            let args = PyTuple::new(py, input);
+            let result = function.call1(py, args)?;
+
+            Ok(())
+            // let f: Py<PyAny> = PyModule::from_code(py, self.code.as_str(), "", "")?
+            //     .getattr("logic")?
+            //     .into();
+            // let values = input.values().into_iter();
+            // let args = PyTuple::new(py, values);
+            // let result = f.call1(py, args);
+            //     ()
         })
     }
+
+    // fn to_data_frame(object: PyObject) -> DataFrame {
+    // }
 }
 
-fn
+
+impl ToPyObject for Data {
+    fn to_object(&self, py: Python<'_>) -> PyObject {
+        match self {
+            Data::Boolean(v) => v.into_py(py),
+            Data::UnsignedInt(v) => v.into_py(py),
+            Data::SignedInt(v) => v.into_py(py),
+            Data::Text(v) => v.into_py(py),
+            _ => panic!("ups")
+        }
+    }
+}
