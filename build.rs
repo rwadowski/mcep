92c0bb9c17bb074e126aba881cae53f85da6994f
Robert Wadowski <robert.wadowski@morgenrete.com>
2024-01-25 22:45:18 +0100

    path is fixed

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     if cfg!(target_os = "macos") {
         pyo3_build_config::add_extension_module_link_args();
         println!(
-            "cargo:rustc-link-search=native=/opt/homebrew/Cellar/python@3.11/3.10.13/Frameworks/Python.framework/Versions/3.11/lib"
+            "cargo:rustc-link-search=native=/opt/homebrew/Cellar/python@3.11/3.11.6/Frameworks/Python.framework/Versions/3.11/lib"
         );
         println!("cargo:rustc-link-lib=dylib=python3.11");
     } else {
